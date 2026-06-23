3751ee106fbc539af1c1fd185c76ccf72aaeebb5
carolina2009 <carolina2009mx@gmail.com>
2026-06-22 23:41:23 -0600

    Sube checkbox

--- src/test/java/tests/MiniTest.java
+++ src/test/java/tests/MiniTest.java
@@ -1,11 +1,13 @@
 package tests;
 
 import base.BaseTest;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import org.junit.jupiter.api.Test;
 
 import static com.microsoft.playwright.Page.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class MiniTest extends BaseTest {
 
@@ -20,6 +22,33 @@ public void loginTest() {
 
     assertTrue(page.locator("text=Logged In successfully").isVisible());
     }
+@Test
+    public void LoginFailedTest() {
+    page.navigate("https://practicetestautomation.com/practice-test-login/");
+
+    page.locator("#username").fill("student");
+    page.locator("#password").fill("Password1");
+    page.locator("#submit").click();
+
+    String actualMessage=page.locator("#error").textContent();
+
+    assertEquals("Your password is invalid!",actualMessage);
+}
+@Test
+    public void checkboxTest() {
+    page.navigate("https://the-internet.herokuapp.com/checkboxes");
+
+   Locator checkbox1 =  page.locator("input[type='checkbox']").nth(0);
+   Locator checkbox2 =  page.locator("input[type='checkbox']").nth(1);
+
+   checkbox1.click();
+   checkbox2.uncheck();
+
+
+   assertTrue(checkbox1.isChecked());
+   assertFalse(checkbox2.isChecked());
+
+}
 
 }
 
